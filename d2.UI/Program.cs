9e30d23c6ad4926018e8397282e34a437a8dfeb8
stormrubio3000 <storm-3000@hotmail.com>
2019-02-19 11:26:09 -0600

    I think this is right.

---
+++ d2.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace d2.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
