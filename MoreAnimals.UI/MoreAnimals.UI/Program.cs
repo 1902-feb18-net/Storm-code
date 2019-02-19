d87936801e29b346768a03853dd5db362df32d9f
stormrubio3000 <storm-3000@hotmail.com>
2019-02-19 15:03:51 -0600

    Adding classes and getting inheritance started.

--- MoreAnimals.UI/MoreAnimals.UI/Program.cs
+++ MoreAnimals.UI/MoreAnimals.UI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MoreAnimals.Library;
+using System;
+
 
 namespace MoreAnimals.UI
 {
@@ -7,6 +9,64 @@ namespace MoreAnimals.UI
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Dog fido1 = new Dog();
+
+            fido1.AnimalId = 1;
+            fido1.Name = "Fido";
+            fido1.Breed = "Doberman";
+
+
+            Dog fido2 = new Dog
+            {
+            AnimalId = 1,
+            Name = "Fido",
+            Breed = "Doberman",
+            };
+
+
+            fido1.GoTo("Park");
+            fido1.MakeNoise();
+
+
+            IAnimal animal = fido1;
+
+            //animal.Breed = "";
+            //Bird bird = (Bird)animal;
+
+            Dog dog3 = (Dog)animal;
+
+
+            int integer = (int)3.4;
+
+            double num = integer;
+
+
+            var animals = new IAnimal[2];
+            animals[0] = fido1;
+            animals[1] = new Eagle
+            {
+                AnimalId = 3,
+                Name = "Bill"
+            };
+
+
+
+            foreach (var item in animals)
+            {
+                Console.WriteLine(item.Name);
+                item.MakeNoise();
+                item.GoTo("Park");
+            }
+
+
+            MakeNoise(dog3);//upcasted to the function below with the static call.
         }
+
+        static void MakeNoise(IAnimal animal)
+        {
+            animal.MakeNoise();
+        }
+
     }
 }
