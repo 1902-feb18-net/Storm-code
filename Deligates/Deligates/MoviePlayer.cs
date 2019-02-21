db1ea4bd262529861712073b29a03cf7f4196b62
stormrubio3000 <storm-3000@hotmail.com>
2019-02-21 14:33:48 -0600

    The delegate example

---
+++ Deligates/Deligates/MoviePlayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Deligates
+{
+    public class MoviePlayer
+    {
+
+        public Movie CurrentMovie { get; set; }
+
+
+
+        public delegate void MovieFinishedHandler();
+
+        public event MovieFinishedHandler MovieFinished;
+        public event Action<string> DiskEjected;
+
+
+
+        public bool Play()
+        {
+            Thread.Sleep(3000);
+
+            if (MovieFinished != null)
+            {
+                MovieFinished();
+            }
+
+            DiskEjected?.Invoke(CurrentMovie.ToString());
+            return true;
+        }
+    }
+}
---
+++ Deligates/Deligates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Deligates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var moviePlayer = new MoviePlayer
+            {
+                CurrentMovie = Movie.StarWars4
+            };
+
+            MoviePlayer.MovieFinishedHandler handler = EjectDisk;
+
+
+
+            moviePlayer.MovieFinished += handler;
+
+
+            moviePlayer.DiskEjected += (s) => Console.WriteLine($"Ejecting {s}");
+
+
+            FuncAndAction();
+
+
+            Console.WriteLine("Playing Movie..");
+
+
+            moviePlayer.Play();
+
+            Console.ReadLine();
+        }
+        public static void EjectDisk()
+        {
+            Console.WriteLine("Ejecting disc.");
+
+        }
+
+
+        public static void FuncAndAction()
+        {
+            Func<string, string, int> func = (s1,s2) => s1.Length + s1.Length;
+            Action<string, string, int> action = (s1, s2, i) => Console.WriteLine(s1 + s2 + i);
+        }
+    }
+}
